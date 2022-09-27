d3cb8cd390f5553228de71fe7f28ecfe57d2b041
David Bareiro <davidbareiro@gmail.com>
2022-09-27 19:11:04 -0300

    segunda

--- otro.js
+++ otro.js
@@ -1,11 +1,27 @@
 
 
 
+
+
+function fncEntrar() {
+
+  console.log (pedido);
+  console.log (deuda);
+ 
+  if (parseFloat(pedido)>= deuda)  {
+    alert("No es posible solicitar mas de la deuda actual");
+  }
+  else 
+    if(parseFloat(pedido)< deuda)  {
+      alert("Es posible entregarle mas dinero");
+  }
+    
+
+}
+
+
 const element = document.getElementById("btnsolicitar");
 element.addEventListener("click", fncEntrar);
 
-function fncEntrar() {
- 
-    alert ("Bienvenido  !");
-    // location.href = "cuentas.html";
-}
+const pedido = document.getElementById("pedido").value;
+const deuda = document.getElementById("deuda").value;
